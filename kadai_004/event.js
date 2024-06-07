08f778f73557ce4c1e69ea4945db1c73290efe53
eigo-sugiyama <eigo.sugiyama0403@gmail.com>
2024-06-07 11:09:20 +0900

    commit until chapter4

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(window).on('load scroll', (e) => {
+    // ロードされた場合
+    if(e.type === 'load'){
+      console.log('loadイベントが発生しました');
+    }
+    // キーが押された場合
+    if(e.type === 'scroll'){
+      console.log('scrollイベントが発生しました');
+    }
+ });
+});
